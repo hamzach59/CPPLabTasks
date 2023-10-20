87339e84d46659ab1f6d82905e2107c579adaf34
Muhammad Hamza <147749542+hamzach59@users.noreply.github.com>
2023-10-20 11:57:39 +0500

    Add files via upload

---
+++ Assignment 1/MUHAMMAD_HAMZA_467023_LABTASK3.cpp
@@ -0,0 +1,64 @@
+//LAB TASK 4
+//
+#include<iostream>
+using namespace std;
+//
+//TASK 1:Find Sum Of First 10 Natural Numbers
+//int main()
+//{
+//	cout<<"Numbers";
+//	int a, b, c,n;
+//	n=0;
+//	for(a=0;a<10;a++){
+//		cin>>b;
+//		n=n+b;
+//	}
+//	cout<<"sum of 10 natural numbers:"<<n<<endl;
+//}
+//
+//TASK 2:Print Table Of Any Number
+//int main()
+//{
+//	int F, N, S;
+//	cout<<"TABLE OF:";
+//	cin>>N;
+//	for(F=1;F<=10;F++){
+//		S=N*F;
+//		cout<<N<<" X "<<F<<" = "<<S<<endl;
+//	}
+//	
+//}
+
+//TASK 3:Generate Factorial Of Any Number
+//int main(){
+//	int A , B, C,F,D;
+//	int E=1;
+//	cout<<"Factorial Of:";
+//	cin>>A;
+//	for(F=1;F<=A;F++){
+//		E=E*F;
+//		
+//	}
+//	cout<<A<<"!="<<E<<endl;
+//		
+//}
+//
+//TASK 4:Generate Fibonacci Sequence upto a certain number
+//int main(){
+//	int x,a,b,sum;
+//	cout<<"STEPS:";
+//	cin>>x;
+//	cout<< "First digit:";
+//	cin >> a;
+//	cout << "Second digit:";
+//	cin >> b;
+//	cout << a << ","<< b<< ",";
+//	for (int i= 0;i<x;i++){
+//		sum = a+b;
+//		cout << "," <<sum ;
+//		a = b;
+//		b = sum;
+//		
+//	}
+//}
+
